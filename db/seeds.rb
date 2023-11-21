5481ef560d643b513924c8460adef80c163ca22f
mikgn <kogan.miha@gmail.com>
2023-11-22 00:12:54 +0400

    feature/implement-setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,131 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+Admin.create!(
+  name: 'admin',
+  email: 'admin@example.com',
+  status: 1,
+  password: 'Adminpass',
+  password_confirmation: 'Adminpass'
+
+)
+
+puts
+puts 'Admin created'
+
+5.times do |i|
+  Merchant.create!(
+    name: "test_merchant#{i}",
+    email: "test_merchant#{i}@example.com",
+    description: "test_merchant#{i} description",
+    status: [0, 1].sample,
+    password: "Testpass#{i}",
+    password_confirmation: "Testpass#{i}"
+  )
+end
+
+puts 'Merchants created'
+
+merchants = Merchant.all
+
+5.times do |i|
+  merchants.each do |merchant|
+    amount = rand(10..100)
+    status = 0
+    customer_email = "#{merchant.name}#{i}@example.com"
+    customer_phone = rand(1111111111..9999999999)
+    user_id = merchant.id
+
+    authorize = Authorize.create!(
+      amount:,
+      status:,
+      customer_email:,
+      customer_phone:,
+      user_id:
+    )
+
+    parent_id = authorize.id
+
+    charge = Charge.create!(
+      amount:,
+      status:,
+      customer_email:,
+      customer_phone:,
+      user_id:,
+      parent_id:
+    )
+  end
+end
+
+puts 'Approved transactions created'
+
+2.times do |i|
+  merchants.each do |merchant|
+    amount = rand(10..100)
+    status = 0
+    customer_email = "#{merchant.name}#{i}@example.com"
+    customer_phone = rand(1111111111..9999999999)
+    user_id = merchant.id
+
+    authorize = Authorize.create!(
+      amount:,
+      status:,
+      customer_email:,
+      customer_phone:,
+      user_id:
+    )
+
+    parent_id = authorize.id
+
+    charge = Reversal.create!(
+      status:,
+      customer_email:,
+      customer_phone:,
+      user_id:,
+      parent_id:
+    )
+  end
+end
+
+puts 'Reversed transactions created'
+
+2.times do |i|
+  merchants.each do |merchant|
+    amount = rand(10..100)
+    status = 0
+    customer_email = "#{merchant.name}#{i}@example.com"
+    customer_phone = rand(1111111111..9999999999)
+    user_id = merchant.id
+
+    authorize = Authorize.create!(
+      amount:,
+      status:,
+      customer_email:,
+      customer_phone:,
+      user_id:
+    )
+
+    parent_id = authorize.id
+
+    charge = Charge.create!(
+      amount:,
+      status:,
+      customer_email:,
+      customer_phone:,
+      user_id:,
+      parent_id:
+    )
+
+    parent_id = charge.id
+
+    charge = Refund.create!(
+      amount:,
+      status:,
+      customer_email:,
+      customer_phone:,
+      user_id:,
+      parent_id:
+    )
+  end
+end
+
+puts 'Refunded transactions created'
