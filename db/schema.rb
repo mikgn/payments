b6fdd3c2b6d417b312aaba8a674b765f760b54bb
mikgn <kogan.miha@gmail.com>
2023-11-21 11:56:13 +0400

    fix/add-unique-index-to-users-email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_08_080100) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_21_075403) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_08_080100) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "transactions", "users"
